a60f5f46dda8b3e0c986925038316ba37ce9b3c1
Barry O'Neill <barra.oneill@gmail.com>
2018-05-01 11:34:58 -0400

    more cleanup

--- src/main/scala/com/hbc/dockerit/TwitterFutureOps.scala
+++ src/main/scala/com/hbc/dockerit/TwitterFutureOps.scala
@@ -1,8 +1,12 @@
 package com.hbc.dockerit
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 object TwitterFutureOps {
 
   import com.twitter.util.{Future => TFuture}
+
   import scala.concurrent.{Promise, Future => SFuture}
 
   implicit class TwitterFutureOps[T](val f: TFuture[T]) extends AnyVal {
@@ -11,5 +15,10 @@ object TwitterFutureOps {
       f.respond(t => promise.complete(t.asScala))
       promise.future
     }
+
+    def result(timeoutSecs: Int = 5): T = {
+      Await.result(f.asScala, timeoutSecs.seconds)
+    }
   }
+
 }
--- src/main/scala/com/hbc/dockerit/matchers/RedisMatchers.scala
+++ src/main/scala/com/hbc/dockerit/matchers/RedisMatchers.scala
@@ -1,38 +1,46 @@
 package com.hbc.dockerit.matchers
 
 import com.hbc.dockerit.containers.RedisContainer
+import com.hbc.dockerit.util.RedisUtil
 import com.twitter.finagle.Redis
 import com.twitter.finagle.redis.Client
-import com.twitter.finagle.redis.util.BufToString
-import com.twitter.io.Buf
 import org.scalatest.Matchers
 import org.scalatest.matchers.{MatchResult, Matcher}
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 trait RedisMatchers extends Matchers {
   container: RedisContainer =>
 
-  import com.hbc.dockerit.TwitterFutureOps._
-
   lazy val redis: Client = Redis.newRichClient(s"localhost:$redisPort")
 
-  def haveKeys(expectedKeys: Seq[String]) = new RedisKeyMatcher(expectedKeys)
+  def haveKeysMatching(pattern: String) = Matcher { (c: Client) =>
+    val actualKeys = RedisUtil(c).keys(pattern)
 
-  class RedisKeyMatcher(expectedKeys: Seq[String]) extends Matcher[Client] {
-
-    def apply(left: Client): MatchResult = {
-
-      val keyFut = redis.keys(Buf.Utf8("*")).map(f => f.map(BufToString(_))).asScala
-      val actualKeys = Await.result(keyFut, 5.seconds)
-
-      MatchResult(
-        actualKeys.size == expectedKeys.size && actualKeys.toSet == expectedKeys.toSet,
-        s"""Expected keys $expectedKeys but got $actualKeys""",
-        s"""Expected keys $expectedKeys but got $actualKeys"""
-      )
-    }
+    MatchResult(actualKeys.nonEmpty,
+      s"""Expected at least one key to match "$pattern" but none was found""",
+      s"""Expected at least one key to match "$pattern" but none was found"""
+    )
   }
+
+  def haveKeys(expectedKeys: String*) = Matcher { (c: Client) =>
+    val actualKeys = RedisUtil(c).keys("*")
+
+    val notFound = expectedKeys.filter(k => !actualKeys.contains(k))
+
+    MatchResult(notFound.isEmpty,
+      s"""Couldn't find key(s) "$notFound" in: $actualKeys""",
+      s"""Couldn't find key(s) "$notFound" in: $actualKeys""",
+    )
+  }
+
+  def haveOnlyKeys(expectedKeys: String*) = Matcher { (c: Client) =>
+    val actualKeys = RedisUtil(c).keys("*")
+
+    MatchResult(
+      actualKeys.size == expectedKeys.size && actualKeys.toSet == expectedKeys.toSet,
+      s"""Expected keys $expectedKeys but got $actualKeys""",
+      s"""Expected keys $expectedKeys but got $actualKeys"""
+    )
+  }
+
 
 }
