218694a4784b15f8648718ade717f58c61193cff
Barry O'Neill <barra.oneill@gmail.com>
2018-05-01 15:32:17 -0400

    done with redis matchers for now

---
+++ src/main/scala/com/hbc/dockerit/util/CirceSupport.scala
@@ -0,0 +1,17 @@
+package com.hbc.dockerit.util
+
+import io.circe.parser.decode
+import io.circe.syntax._
+
+trait CirceSupport {
+
+  def decodeOrThrow[T](value: String)(implicit decoder: io.circe.Decoder[T]): T =
+
+    decode[T](value) match {
+      case Right(t: T) => t
+      case Left(e) => throw new RuntimeException(s"""Couldn't parse "$value"""", e)
+    }
+
+  def encode[T](value: T)(implicit decoder: io.circe.Encoder[T]): String = value.asJson.noSpaces
+
+}
--- src/main/scala/com/hbc/dockerit/util/RedisUtil.scala
+++ src/main/scala/com/hbc/dockerit/util/RedisUtil.scala
@@ -4,17 +4,26 @@ import com.twitter.finagle.redis.Client
 import com.twitter.finagle.redis.util.BufToString
 import com.twitter.io.Buf
 
+
 /**
   * A wrapper for the redis client, providing a bunch of synchronous convenience
   * methods to save clutter in tests/matchers.
   */
-case class RedisUtil(client: Client) {
+case class RedisUtil(client: Client) extends CirceSupport {
 
   import com.hbc.dockerit.TwitterFutureOps._
 
   def resetCache(timeoutSecs: Int = 5): Unit = {
     client.flushAll().result(timeoutSecs)
   }
+
+  /**
+    * Call 'keys', returning those that match the provided pattern
+    */
+  def keys(pattern: String, timeoutSecs: Int = 5): Seq[String] = {
+    client.keys(Buf.Utf8(pattern)).map(_.map(BufToString(_))).result(timeoutSecs)
+  }
+
 
   /**
     * Call 'mset' with the provided data as UTF8.  Returns self reference for chaining
@@ -25,11 +34,13 @@ case class RedisUtil(client: Client) {
     client.mSet(redisVals).result(timeoutSecs)
   }
 
-  /**
-    * Call 'keys', returning those that match the provided pattern
-    */
-  def keys(pattern: String, timeoutSecs: Int = 5): Seq[String] = {
-    client.keys(Buf.Utf8(pattern)).map(_.map(BufToString(_))).result(timeoutSecs)
-  }
+  def get(key: String, timeoutSecs: Int = 5): Option[String] =
+    client.get(Buf.Utf8(key)).result(timeoutSecs).map(BufToString(_))
+
+  def getDecoded[T](key: String, timeoutSecs: Int = 5)(implicit decoder: io.circe.Decoder[T]): Option[T] =
+    get(key, timeoutSecs).map(v => decodeOrThrow[T](v))
+
+  def set[T](key: String, value: T, timeoutSecs: Int = 5)(implicit encoder: io.circe.Encoder[T]): Unit =
+    client.set(Buf.Utf8(key), Buf.Utf8(encode(value))).result(timeoutSecs)
 
 }
