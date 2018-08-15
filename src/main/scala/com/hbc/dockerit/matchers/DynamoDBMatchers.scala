ca89d611e88a94acc109913c6ff8c797e1f98888
Barry O'Neill <barra.oneill@gmail.com>
2018-08-15 09:57:17 -0400

    more thorough tests

--- src/main/scala/com/hbc/dockerit/matchers/DynamoDBMatchers.scala
+++ src/main/scala/com/hbc/dockerit/matchers/DynamoDBMatchers.scala
@@ -10,25 +10,38 @@ trait DynamoDBMatchers extends Matchers {
   container: DynamoDBContainer =>
 
   def beAvailable() = Matcher { client: DynamoDB =>
-
     MatchResult(DynamoDBUtil(client).ping,
-      s"""DynamoDB does not appear to be available""",
-      s"""DynamoDB is available""")
+                s"""DynamoDB does not appear to be available""",
+                s"""DynamoDB is available""")
 
   }
 
   def haveTable(tableName: String) = Matcher { client: DynamoDB =>
-
     DynamoDBUtil(client).tableExists(tableName) match {
 
-        case Right(tableExists) =>
-              MatchResult(tableExists,
-                          s"""Expected table "$tableName" to exist but it does not""",
-                          s"""Table: "$tableName" exists""")
+      case Right(tableExists) =>
+        MatchResult(tableExists,
+                    s"""Expected table "$tableName" to exist but it does not""",
+                    s"""Did not expect table "$tableName" to exist""")
 
-            case Left(e) =>
-              val msg = s"""Error looking up table $tableName: $e"""
-              MatchResult(matches = false, msg, msg)
+      case Left(e) =>
+        val msg = s"""Error looking up table $tableName: $e"""
+        MatchResult(matches = false, msg, msg)
+    }
+
+  }
+
+  def haveItemCount(tableName: String, expected: Long) = Matcher { client: DynamoDB =>
+    DynamoDBUtil(client).getItemCount(tableName) match {
+
+      case Right(actual) =>
+        MatchResult(actual == expected,
+                    s"""Table "$tableName" has unexpected itemCount $actual (expected $expected)""",
+                    s"""Table "$tableName" not expected to have itemCount $expected but it did""")
+
+      case Left(e) =>
+        val msg = s"""Error looking up table $tableName: $e"""
+        MatchResult(matches = false, msg, msg)
     }
 
   }
